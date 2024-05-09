346995f5146ed14500330f4de291027e01bf1926
bhavin-stablelogic <bhavin.nakrani@stablelogic.com>
2024-05-09 15:59:30 +0530

    workflow component added

---
+++ src/routes/veroxosSupport/index.tsx
@@ -0,0 +1,128 @@
+'use client'
+
+import { useEffect, useRef, useState } from "react";
+import WorkflowLayout, {
+	getWorkflowRequestTypesApiCall,
+  } from '@veroxos/workflow/lib/src/index_web';
+import { jsonObject } from './workflowdata';
+  
+
+const VeroxosSupportPage = () => {
+  //console.log(JSON.stringify(jsonObject))
+	const [dynamicComponent, setDynamicComponent] = useState(jsonObject);
+	  //@ts-ignore
+	  const ref = useRef();
+
+	  // useEffect(() => {
+		// getWorkflowRequestTypesApiCall(
+		//   	"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2NTU4NzQzODgsImV4cCI6MTY1NjQ3OTE4OCwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoibmlrdW5qLnZhc2FuaUBzdGFibGVsb2dpYy5jb20ifQ.0RhTVOg6cYXgyw8dCR0C4flBhb18zCkIIbJLIhGV9msMU_ok0tFEJOaoBulImEjW8Jkun_2N1yiWrTei3HL7X_YrATx0f0SA7_BqOgSREzPxzmFU0zT9t5T2nmlaYYNgwe1vB8rTtqKxnhEIndLIPlwddAvt-I8CbFiGcQKJM_YTQAsPxX2_H2csORO2FZ8zd5Dip1AI5oUG4qKhpklLmUnmY28KghkiW6UShUNj8KmFMpTgAGacmBpzAzuctcVy-LVbEHGnexEEDaPkBVU7bq8Qt-u0HX9EEDNrkJL6T64cN13I4kI7-CjBR8erhd5aQn-EcTYVKRw4yXB7_dtRag",
+		// 	362
+		// ).then((resultData) => {
+		//   // eslint-disable-next-line no-restricted-syntax
+		//   //console.log(JSON.stringify(resultData?.data));
+		//   setDynamicComponent(resultData?.data);
+		// });
+	  // }, []);
+    console.log(ref);
+	return <div>
+		{/* <div style={{ marginTop: "10px" }}>
+        <button
+          onClick={() => {
+            ref.current.refreshNow();
+          }}
+        >
+          RefreshNow-All
+        </button>
+        <button
+          style={{ marginLeft: "10px" }}
+          onClick={() => {
+            // eslint-disable-next-line no-restricted-syntax
+            console.log(ref.current.isValidate());
+          }}
+        >
+          Validate-All
+        </button>
+        <button
+          style={{ marginLeft: "10px" }}
+          onClick={() => {
+            ref.current.setValue({
+              title: "Update from set value method by all",
+              value: "Update from set value method by all",
+            });
+          }}
+        >
+          SetValue-All
+        </button>
+        <button
+          style={{ marginLeft: "10px" }}
+          onClick={() => {
+            // eslint-disable-next-line no-restricted-syntax
+            console.log(ref.current.getValue());
+          }}
+        >
+          GetValue-All
+        </button>
+      </div> */}
+
+	  <div style={{ marginTop: "10px" }}>
+        {/* <button
+          onClick={() => {
+            ref.current.refreshNow("af37fc63-0861-483b-ab30-e61294229077");
+          }}
+        >
+          RefreshNow-Id
+        </button>
+        <button
+          style={{ marginLeft: "10px" }}
+          onClick={() => {
+            // eslint-disable-next-line no-restricted-syntax
+            console.log(
+              ref.current.isValidate("af37fc63-0861-483b-ab30-e61294229077")
+            );
+          }}
+        >
+          Validate-Id
+        </button> */}
+        {/* <button
+          style={{ marginLeft: "10px" }}
+          onClick={() => {
+            ref.current.setValue(
+              {
+                title: "Update from set value method by all",
+                value: "Update from set value method by id",
+              },
+              "16b3829b-404f-4743-9142-fd1ea48a0ba3"
+            );
+          }}
+        >
+          SetValue-Id
+        </button> */}
+        <button
+          style={{ marginLeft: "10px" }}
+          onClick={() => {
+            // eslint-disable-next-line no-restricted-syntax
+            console.log(
+              ref.current.getValue("16b3829b-404f-4743-9142-fd1ea48a0ba3")
+            );
+          }}
+        >
+          GetValue-Id
+        </button>
+        <button
+          style={{ marginLeft: "10px" }}
+          onClick={() => {
+            // eslint-disable-next-line no-restricted-syntax
+            console.log(ref.current.getValue());
+          }}
+        >
+          GetValue-All
+        </button>
+      </div>
+
+	  <div>
+		<WorkflowLayout ref={ref} workflowId={362} data={dynamicComponent} />
+		</div>
+	</div>
+}
+
+export default VeroxosSupportPage
---
+++ src/routes/veroxosSupport/workflowdata/index.js
@@ -0,0 +1,30 @@
+export const jsonObject = {
+    "16b3829b-404f-4743-9142-fd1ea48a0ba3": {
+        id: "16b3829b-404f-4743-9142-fd1ea48a0ba3",
+        type: "inputBox",
+        parentType: "inputBox",
+        title: "Input Box:",
+        order: "7",
+        group: "0",
+        properties: {
+          placerHolder: {
+            value: "placeholder value",
+            display: "placeholder display"
+          },
+          MANDATORY_STATUS: {
+            value: "1",
+            display: "Yes"
+          },
+          "marginTop": {
+            "value": "10",
+            "display": "50"
+          },
+          "marginBelow": {
+            "value": "10",
+            "display": "50"
+          },
+        },
+        value: "",      
+        tag: ""
+    },
+};
