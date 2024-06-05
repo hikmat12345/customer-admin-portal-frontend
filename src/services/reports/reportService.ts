9a9bcd559501f3b2468d17eb3898e403eb5921f1
noman.monis <noman.monis@stablelogic.com>
2024-06-05 11:01:16 +0500

    fix: header in report service for testing

--- src/services/reports/reportService.ts
+++ src/services/reports/reportService.ts
@@ -2,58 +2,33 @@ import { NEXT_PUBLIC_REPORTING_SERVICE_URL } from 'config/config'
 import httpClient from '../httpClient'
 
 export const postF1Report = async (data: any) => {
-	const headers = {
-		'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-	}
-
-	return httpClient.post(`${NEXT_PUBLIC_REPORTING_SERVICE_URL}/F1`, data, { headers }).then(({ data }) => data)
+	return httpClient.post(`${NEXT_PUBLIC_REPORTING_SERVICE_URL}/F1`, data).then(({ data }) => data)
 }
 
 export const postF3Report = async (data: any) => {
-	const headers = {
-		'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-	}
-	return httpClient.post(`${NEXT_PUBLIC_REPORTING_SERVICE_URL}/F3`, data, { headers }).then(({ data }) => data)
+	return httpClient.post(`${NEXT_PUBLIC_REPORTING_SERVICE_URL}/F3`, data).then(({ data }) => data)
 }
 
 export const postF4Report = async (data: any) => {
-	const headers = {
-		'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-	}
-	return httpClient.post(`${NEXT_PUBLIC_REPORTING_SERVICE_URL}/F4`, data, { headers }).then(({ data }) => data)
+	return httpClient.post(`${NEXT_PUBLIC_REPORTING_SERVICE_URL}/F4`, data).then(({ data }) => data)
 }
 
 export const postF5Report = async (data: any) => {
-	const headers = {
-		'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-	}
-	return httpClient.post(`${NEXT_PUBLIC_REPORTING_SERVICE_URL}/F5`, data, { headers }).then(({ data }) => data)
+	return httpClient.post(`${NEXT_PUBLIC_REPORTING_SERVICE_URL}/F5`, data).then(({ data }) => data)
 }
 
 export const postF6Report = async (data: any) => {
-	const headers = {
-		'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-	}
-	return httpClient.post(`${NEXT_PUBLIC_REPORTING_SERVICE_URL}/F6`, data, { headers }).then(({ data }) => data)
+	return httpClient.post(`${NEXT_PUBLIC_REPORTING_SERVICE_URL}/F6`, data).then(({ data }) => data)
 }
 
 export const postF7Report = async (data: any) => {
-	const headers = {
-		'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-	}
-	return httpClient.post(`${NEXT_PUBLIC_REPORTING_SERVICE_URL}/F7`, data, { headers }).then(({ data }) => data)
+	return httpClient.post(`${NEXT_PUBLIC_REPORTING_SERVICE_URL}/F7`, data).then(({ data }) => data)
 }
 
 export const postF12Report = async (data: any) => {
-	const headers = {
-		'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-	}
-	return httpClient.post(`${NEXT_PUBLIC_REPORTING_SERVICE_URL}/F12`, data, { headers }).then(({ data }) => data)
+	return httpClient.post(`${NEXT_PUBLIC_REPORTING_SERVICE_URL}/F12`, data).then(({ data }) => data)
 }
 
 export const postF15Report = async (data: any) => {
-	const headers = {
-		'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-	}
-	return httpClient.post(`${NEXT_PUBLIC_REPORTING_SERVICE_URL}/F15`, data, { headers }).then(({ data }) => data)
+	return httpClient.post(`${NEXT_PUBLIC_REPORTING_SERVICE_URL}/F15`, data).then(({ data }) => data)
 }
