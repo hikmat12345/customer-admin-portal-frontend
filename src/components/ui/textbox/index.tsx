1b74c795014ed5b8afe0ae6a01010a1c42db1471
Hikmat Ullah <hikmat.ullah@teo-intl.com>
2024-07-03 18:51:57 +0500

    fux: resolved commits

--- src/components/ui/textbox/index.tsx
+++ src/components/ui/textbox/index.tsx
@@ -16,7 +16,7 @@ const TooltipText: FC<TooltipTextProps> = ({ text, maxLength = 10, className, ty
   return shouldTruncate ? (
     <TooltipProvider>
       <Tooltip>
-        <TooltipTrigger className={`relative ${className} m-0 p-0`}>
+        <TooltipTrigger className={`relative ${className}`}>
           {type === 'notification' ? (
             <Image src="/notification.svg" alt="info" width={16} height={16} />
           ) : (
