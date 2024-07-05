4b7b7d36eb8cf3d3d5a7dffb0798b7f22d0e6639
Hassan Ibrahim <hassan.ibrahim@stablelogic.com>
2024-07-05 13:08:02 +0500

    bugfix: VX2-435

--- src/views/account-detail/components/account-general-info/index.tsx
+++ src/views/account-detail/components/account-general-info/index.tsx
@@ -53,60 +53,58 @@ export default function AccountGeneralInfo({
         <div className="text-nowrap pb-6 sm:block sm:gap-x-[1rem] xl:flex xl:gap-x-[2.1rem] 2xl:gap-x-[3.75rem]">
           <div className="sm:block sm:w-[100%] lg:flex xl:w-[80%]">
             <div className="flex justify-between sm:w-[100%] lg:w-[50%] lg:gap-x-[2rem] xl:gap-x-[2.1rem] 2xl:gap-x-[3.75rem] max-lg:w-[100%]">
-              <div className="sm:w-[50%] lg:w-[40%]">
+              <div className="leading-6 sm:w-[50%] lg:w-[40%]">
                 {staticData.slice(0, 7).map((item, index) => (
-                  <div key={index} className="text-[0.875rem] font-[600] leading-6 text-[#000]">
+                  <div key={index} className="text-[0.875rem] font-[600] text-[#000]">
                     {item.label}
                   </div>
                 ))}
               </div>
-              <div className="sm:w-[50%] sm:pl-4 lg:w-[60%] lg:pl-0">
+              <div className="leading-normal sm:w-[50%] sm:pl-4 lg:w-[60%] lg:pl-0 2lg:leading-6">
                 {staticData.slice(0, 7).map((item, index) => (
-                  <div key={index} className="">
+                  <div key={index}>
                     {typeof item.value !== 'undefined' ? (
                       typeof item.value === 'boolean' ? (
-                        <div className="text-[0.875rem] leading-6 text-[#575757]">
-                          {item.value ? item.value : ' - '}
-                        </div>
+                        <div className="text-[0.875rem] text-[#575757]">{item.value ? item.value : ' - '}</div>
                       ) : (
                         <TooltipText
                           text={item.value ? item.value : ' - '}
                           maxLength={27}
-                          className="text-[0.875rem] leading-6 text-[#575757]"
+                          className="text-[0.875rem] text-[#575757]"
                         />
                       )
                     ) : (
-                      <div className="text-[0.875rem] leading-6 text-[#575757]">-</div>
+                      <div className="text-[0.875rem] text-[#575757]">-</div>
                     )}
                   </div>
                 ))}
               </div>
             </div>
-            <div className="flex justify-center pr-3 sm:mt-5 sm:w-[100%] lg:mt-0 lg:w-[50%] lg:gap-x-[2rem] xl:gap-x-[2.1rem] 2xl:gap-x-[3.75rem] max-lg:mt-5 max-lg:w-[100%]">
-              <div className="sm:w-[50%] lg:w-[70%]">
+            <div className="flex justify-center sm:mt-5 sm:w-[100%] lg:mt-0 lg:w-[50%] lg:gap-x-[2rem] xl:gap-x-[2.1rem] 2xl:gap-x-[3.75rem] max-lg:mt-5 max-lg:w-[100%]">
+              <div className="leading-6 sm:w-[50%] lg:w-[70%]">
                 {staticData.slice(7).map((item, index) => (
-                  <div key={index} className="text-[0.875rem] font-[600] leading-6 text-[#000]">
+                  <div key={index} className="text-[0.875rem] font-[600] text-[#000]">
                     {item.label}
                   </div>
                 ))}
               </div>
-              <div className="sm:w-[50%] sm:pl-4 lg:w-[30%] lg:pl-0">
+              <div className="leading-normal sm:w-[50%] sm:pl-4 lg:w-[30%] lg:pl-0 2lg:leading-6">
                 {staticData.slice(7).map((item, index) => (
                   <div key={index}>
                     {typeof item.value !== 'undefined' ? (
                       typeof item.value === 'boolean' ? (
-                        <div className="text-left text-[0.875rem] leading-6 text-[#575757]">
+                        <div className="text-left text-[0.875rem] text-[#575757]">
                           {item.value ? item.value : ' - '}
                         </div>
                       ) : (
                         <TooltipText
                           text={item.value || '-'}
                           maxLength={13}
-                          className="text-left text-[0.875rem] leading-6 text-[#575757]"
+                          className="text-left text-[0.875rem] text-[#575757]"
                         />
                       )
                     ) : (
-                      <div className="text-[0.875rem] leading-6 text-[#575757]">-</div>
+                      <div className="text-[0.875rem] text-[#575757]">-</div>
                     )}
                   </div>
                 ))}
